16c7a0448b348c1ad71ae5f60e6f4063d21c0811
anilpendela <62790014+anilpendela@users.noreply.github.com>
2020-12-05 14:18:29 +0530

    Update asgi.py

--- my_django_project/asgi.py
+++ my_django_project/asgi.py
@@ -8,7 +8,6 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 import os, copy
->>>>>>> 3e83ea5c3e6fa64eaeb0835d66618cab06965291
 
 from django.core.asgi import get_asgi_application
 
